ca70cff675d23f7841d20385931dcc32bdd95373
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-19 01:54:12 +0530

    implemented connection request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,6 +4,7 @@ const requestRouter  = express.Router();
 
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequestModel = require('../models/connectionRequest');
+const User = require('../models/user');
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth , async (req,res) =>{
 
@@ -19,7 +20,25 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth , async (req,res)
                 message:"Invalid Status type: " +status
             })
           }
+
+          const toUser = await User.findById(toUserId);
+          if(!toUser){
+            return res.status(400).json({
+                message:"User Not Found!! "
+          })
+        }
           
+          const existingConnectionRequest = await ConnectionRequestModel.findOne({
+            $or: [
+                {    fromUserId, toUserId },
+                {    fromUserId: toUserId , toUserId:fromUserId},
+                ]
+            });
+
+        if(existingConnectionRequest){
+            return res.status(400).send({message :"Connection Request Already Existed"});
+        }
+
           const connectionRequest = new ConnectionRequestModel({
             fromUserId,
             toUserId,
