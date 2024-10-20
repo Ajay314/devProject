81c75b9269ff9a44b2c4f22b560d619da789ca15
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-20 23:18:59 +0530

    created get user connection API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require('../middlewares/auth');
 const ConnectionRequestModel = require('../models/connectionRequest');
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName age gender skills";
 
 userRouter.get("/user/requests", userAuth, async (req,res)=>{
 
@@ -11,10 +12,28 @@ userRouter.get("/user/requests", userAuth, async (req,res)=>{
         const loggesInUser = req.user;
         const connectionRequests = await ConnectionRequestModel.find({
             toUserId:loggedInUser._id,
+            status:"interested"
+        }).populate("fromUserId",["firstName","lastName"]);
 
-        }) 
         res.json({message: "Data fetched" , 
             data:connectionRequests});
+    }
+    catch(err){
+        res.status(400).send("ERROR:" + err.message);
+    }
+})
+
+userRouter.get("/user/connections" , userAuth, async (req,res)=>{
+
+    try{
+
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                {toUserId:loggedInUser._id, status :"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA);
     }
     catch(err){
         res.status(400).send("ERROR:" + err.message);
