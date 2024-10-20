dc07244d3e3cb76021ffa0abec136b04f0a00031
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-20 22:37:32 +0530

    get user request API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,25 @@
 const express = require('express');
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequestModel = require('../models/connectionRequest');
 const userRouter = express.Router();
 
+
+userRouter.get("/user/requests", userAuth, async (req,res)=>{
+
+    try{
+
+        const loggesInUser = req.user;
+        const connectionRequests = await ConnectionRequestModel.find({
+            toUserId:loggedInUser._id,
+
+        }) 
+        res.json({message: "Data fetched" , 
+            data:connectionRequests});
+    }
+    catch(err){
+        res.status(400).send("ERROR:" + err.message);
+    }
+})
+
+
+module.exports = userRouter;
